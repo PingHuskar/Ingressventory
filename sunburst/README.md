cc1c6921e69c741c9facb31cf91b52551de4d59c
PingHuskar <ping47024@icloud.com>
2023-01-17 23:28:00 +0700

    add sunburst

---
+++ sunburst/README.md
@@ -0,0 +1,5 @@
+# Ingressventory Sunburst
+
+# Library
+- [D3](https://d3js.org/)
+- [d3-sunburst](https://github.com/mojoaxel/d3-sunburst)
---
+++ sunburst/app.js
@@ -0,0 +1,29 @@
+const data = []
+const searchParams = new URLSearchParams(location.search)
+for (const [key, value] of searchParams.entries()) {
+    data.push([key,value])
+}
+const sunburst = new Sunburst({
+    colors: {
+        "res": "#28f428",
+        "enl": "#00c2ff",
+        "key": "#F6BC33",
+        "nkey": "gray",
+        "def": "lime",
+        "atk": "red",
+        "cmn": "#0FAE76",
+        "rare": "#753E71",
+        "xr": "pink",
+        "apex": "#950007",
+        "flip": "brown",
+        "hyp": "#BC119F",
+        "l1": "#fece5a",
+        "l2": "#ffa630",
+        "l3": "#ff7315",
+        "l4": "#e40000",
+        "l5": "#fd2992",
+        "l6": "#eb26",
+        "l7": "#c124e0",
+        "l8": "#9627f4",
+    }
+}).setData(data)
